ea4a8e5eb92b862251368885e2d19aab2a4a3a05
Ahmed Razick <ahmedtitan@gmail.com>
2020-12-27 20:50:17 +0530

    home page content added

--- src/components/Home/HomeContent.js
+++ src/components/Home/HomeContent.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Grid, Typography, CardMedia, Card } from "@material-ui/core";
+import { Grid, Typography, Avatar, Card } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 
 import GridItemWithSwiper from "../common/GridItemWithSwiper";
 import OutlinedButtons from "../common/Button/OutlinedButtons";
+import SocialIcons from "./SocialIcons";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > span": {
+      margin: theme.spacing(2),
+    },
+  },
   sentImage: {
     height: "13em",
   },
@@ -20,13 +26,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const HomeContent = () => {
+  const classes = useStyles();
   return (
     <Grid container spacing={3}>
       <Grid item xs={3}>
-        <GridItemWithSwiper xs={3} swiperParallax="-100">
-          Slide #1
+        <GridItemWithSwiper xs={12} swiperParallax="-100">
+          <Typography variant="h3" gutterBottom>
+            Hi! I'm Nigga
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            Hi! I'm Nigga
+          </Typography>
         </GridItemWithSwiper>
-        <GridItemWithSwiper xs={12} swiperParallax="-200">
+        <GridItemWithSwiper xs={12} swiperParallax="-300">
+          <img
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqnePyQ74Es_GIewC2v2Q-mY3oiBjJ8c1xPg&usqp=CAU"
+            }
+          />
         </GridItemWithSwiper>
       </Grid>
       <Grid item xs={7}>
@@ -39,15 +56,15 @@ const HomeContent = () => {
               Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
             </p>
           </GridItemWithSwiper>
-          <GridItemWithSwiper xs={12} swiperParallax="-200">
-            <Typography>Image Icons</Typography>
+          <GridItemWithSwiper xs={12} swiperParallax="-300">
+            <SocialIcons />
           </GridItemWithSwiper>
           <Grid item xs={12}>
             <Grid container>
-              <GridItemWithSwiper xs={3} swiperParallax="-300">
+              <GridItemWithSwiper xs={3} swiperParallax="-500">
                 <OutlinedButtons color="primary" label="CHECKOUT MY RESUME" />
               </GridItemWithSwiper>
-              <GridItemWithSwiper xs={3} swiperParallax="-300">
+              <GridItemWithSwiper xs={3} swiperParallax="-500">
                 <OutlinedButtons color="primary" label="CONTACT ME" />
               </GridItemWithSwiper>
             </Grid>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import App from "./App";
 // css
-import 'swiper/css/swiper.css';// scss
-import 'swiper/swiper.scss';// less
-import 'swiper/swiper.less';
-import './index.css';
-import './animate.css';
-import { CustomThemeProvider } from './themes/customThemeProvider';
+import "swiper/css/swiper.css"; // scss
+import "swiper/swiper.scss"; // less
+import "swiper/swiper.less";
+import "./index.css";
+import "./animate.css";
+import CustomThemeProvider from "./themes/customThemeProvider";
 
 ReactDOM.render(
   <CustomThemeProvider>
     <CssBaseline />
     <App />
   </CustomThemeProvider>,
-  document.querySelector('#root'),
-)
+  document.querySelector("#root")
+);
